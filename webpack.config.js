4b8db46f2cdd8206370b4cc85981d25bf9dcc9d7
Andrew Piper <pipera88@gmail.com>
2017-05-10 16:12:41 -0700

    webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,114 @@
 var path = require("path");
 var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
+var merge = require('webpack-merge');
+var ENV = process.env.NODE_ENV || 'development';
 
-const DEVELOPMENT = process.env.NODE_ENV === 'development';
-const PRODUCTION = process.env.NODE_ENV === 'production';
-
-var entry = PRODUCTION
-  ? ["./src/index.js"]
-  : [
-    "./src/index.js",
-    "webpack/hot/dev-server",
-    "webpack-dev-server/client?http://localhost:8090"
-  ];
-
-var plugins = PRODUCTION
-  ? []
-  : [
-    new webpack.HotModuleReplacementPlugin()
-  ];
-
-module.exports = {
-  devtool: 'source-map',
-  entry: entry,
-  output: {
-    filename: "bundle.js",
-    publicPath: '/dist/',
-    path: path.resolve(__dirname, "dist")
+var common = {
+  entry: {
+    'polyfills': 'babel-polyfill',
+    'index': './src/index.js',
   },
-  plugins: plugins,
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: './dist/',
+    sourceMapFilename: '[name].map'
+  },
+
+  resolve: {
+    extensions: ['.js',],
+    modules: [path.join(__dirname, 'src'), 'node_modules']
+  },
+
   module: {
-    rules: [
+    loaders: [
       {
         test: /\.(html|svelte)$/,
         exclude: /node_modules/,
         use: "svelte-loader"
       },
       {
-        test: /\.(js|jsx)$/,
-        use: "babel-loader"
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["es2015", "stage-0"]
+          }
+        }
       }
     ]
-  }
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['polyfills', 'vendor'].reverse()
+    }),
+
+    new HtmlWebpackPlugin({
+      chunksSortMode: 'dependency'
+    })
+  ]
 };
+
+var config;
+if (ENV === 'production') {
+  // production
+  config = merge(common, {
+     plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        //mangle: {
+        //  screw_ie8: true,
+        //  keep_fnames: true
+        //},
+        comments: false
+      })
+    ],
+  });
+} else {
+  // development
+  config = merge(common, {
+    devtool: 'cheap-module-source-map',
+
+    entry:  [
+      "./src/index.js",
+      "webpack/hot/dev-server",
+      "webpack-dev-server/client?http://localhost:8090"
+    ],
+
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+      publicPath: './dist/',
+      sourceMapFilename: '[name].map'
+    },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+    
+    devServer: {
+      port: 7777,
+      host: 'localhost',
+      historyApiFallback: true,
+      noInfo: false,
+      stats: 'minimal',
+      publicPath: './dist/', 
+    }
+  });
+}
+
+module.exports = config;
